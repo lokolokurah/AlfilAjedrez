4f37cd925052ab8236fcb3c5c1b846dcf0ad89c6
crazy <crazy@DESKTOP-M0M8881>
2020-11-13 12:45:26 +0100

    4. Clase Posicion Modificada -> Añadidos getters y setters + excepciones

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -1,6 +1,38 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
 public class Posicion {
+	
 	private int fila;
 	private char columna;
+	
+	private void setFila(int fila) {
+		if (fila>0 && fila<9)
+		{
+			this.fila=fila;
+		}
+		else 
+		{
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+	}
+	
+	public int getFila() {
+		return fila;
+	}
+	
+	private void setColumna(char columna) {
+		if (columna == 'a' || columna == 'b' || columna == 'c' || columna == 'd' || columna == 'e' || columna == 'f' || columna == 'g' || columna == 'h')
+		{
+			this.columna = columna;
+		}
+		else
+		{
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}  
+	}
+	
+	public char getColumna() {
+		return columna;
+	}
+	
 }
