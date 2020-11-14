447dd82708d8256ca07d31c3436de411bd9de171
crazy <crazy@DESKTOP-M0M8881>
2020-11-14 13:05:46 +0100

    14. Clase Alfil Modiciada -> Método mover + Global Fix

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -1,10 +1,12 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Alfil {
 
 	private Color color;
 	private Posicion posicion;
-	
+
 	public Alfil() {
 		setColor(Color.NEGRO);
 		setPosicion(new Posicion(8, 'f'));
@@ -24,21 +26,21 @@ public class Alfil {
 			break;
 		}
 	}
-	
+
 	public Alfil(Color color, char columnaInicial) {
 		setColor(color);	
 		if (color==Color.BLANCO)
 		{
-				setPosicion(new Posicion(1, columnaInicial));
-				setColor(color.BLANCO);
+			setPosicion(new Posicion(1, columnaInicial));
+			setColor(color.BLANCO);
 		}
 		if (color==Color.NEGRO) 
 		{
-				setPosicion(new Posicion(8, columnaInicial));
-				setColor(color.NEGRO);
+			setPosicion(new Posicion(8, columnaInicial));
+			setColor(color.NEGRO);
 		}
 	}
-	
+
 	public void setPosicion(Posicion posicion) {
 		if (posicion==null) 
 		{
@@ -48,7 +50,7 @@ public class Alfil {
 		{
 			throw new IllegalArgumentException("ERROR: Fila no válida.");
 		} 
-		else if (posicion.getColumna()!='c' && posicion.getColumna()!='f') 
+		else if (posicion.getColumna()<'a' || posicion.getColumna()>'h') 
 		{
 			throw new IllegalArgumentException("ERROR: Columna no válida.");
 		} 
@@ -57,11 +59,11 @@ public class Alfil {
 			this.posicion=posicion;
 		}
 	}
-	
+
 	public Posicion getPosicion() {
 		return posicion;
 	}
-	
+
 	public void setColor(Color color) {
 		if (color==null) 
 		{
@@ -76,10 +78,70 @@ public class Alfil {
 			this.color=color;
 		}
 	}
-	
+
 	public Color getColor() {
 		return color;
 	}
-	
-	
+
+	public void mover(Direccion direccion, int cantidadDePasos) throws OperationNotSupportedException { 
+		if (direccion == null) 
+		{
+			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+		}
+		if (cantidadDePasos<=0)
+		{
+			throw new IllegalArgumentException("ERROR: El número de pasos debe ser positivo.");
+		}
+		switch (direccion) {
+		case ARRIBA_DERECHA:
+			try 
+			{
+				if (direccion == Direccion.ARRIBA_DERECHA) 
+				{
+					setPosicion(new Posicion(posicion.getFila()+cantidadDePasos,
+							(char) (posicion.getColumna()+cantidadDePasos)));
+				} 			
+			}catch (IllegalArgumentException e) {
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			}
+			break;	
+		case ABAJO_DERECHA:
+			try 
+			{
+				if (direccion == Direccion.ABAJO_DERECHA) 
+				{
+					setPosicion(new Posicion(posicion.getFila()-cantidadDePasos,
+							(char) (posicion.getColumna()+cantidadDePasos)));
+				} 			
+			}catch (IllegalArgumentException e) {
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			}
+			break;	
+		case ABAJO_IZQUIERDA:
+			try 
+			{
+				if (direccion == Direccion.ABAJO_IZQUIERDA) 
+				{
+					setPosicion(new Posicion(posicion.getFila()-cantidadDePasos,
+							(char) (posicion.getColumna()-cantidadDePasos)));
+				} 			
+			}catch (IllegalArgumentException e) {
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			}
+			break;	
+		case ARRIBA_IZQUIERDA:
+			try 
+			{
+				if (direccion == Direccion.ARRIBA_IZQUIERDA) 
+				{
+					setPosicion(new Posicion(posicion.getFila()+cantidadDePasos,
+							(char) (posicion.getColumna()-cantidadDePasos)));
+				} 			
+			}catch (IllegalArgumentException e) {
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			}
+			break;	
+		}
+	}
+
 }
